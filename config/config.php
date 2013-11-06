8dc393c57989ee73f7a646a63c532dccf73e94d8
Preethi <preethi_rajasekar16@yahoo.com>
2013-11-05 20:04:17 -0800

    Updated Config

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p2.preethiwebapp.biz'); 
+	define('DB_NAME', 'preethiw_p2_preethiwebapp_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
